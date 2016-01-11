84b1cdd90550f0884b2f8762927d10fc77d14b6e
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:33:37 +0100

    attributes

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/WriteActivity.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/WriteActivity.java
@@ -1,9 +1,15 @@
 package neonlight88.com.nfctools;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class WriteActivity extends ActionBarActivity {
+public class WriteActivity extends Activity {
+
+    private final String MIMETYPE = "*/*";
+    private NfcAdapter nfcAdapter;
+    private EditText et1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
