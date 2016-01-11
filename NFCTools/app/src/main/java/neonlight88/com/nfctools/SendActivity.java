f95f6a3a99f4954cd69c7f17ca04e7fed5061db9
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:46:18 +0100

    attributes

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/SendActivity.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/SendActivity.java
@@ -3,9 +3,13 @@ package neonlight88.com.nfctools;
 import android.app.Activity;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class SendActivity extends Activity
         implements NfcAdapter.CreateNdefMessageCallback, NfcAdapter.OnNdefPushCompleteCallback {
+
+    private NfcAdapter nfcAdapter;
+    private EditText et2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
