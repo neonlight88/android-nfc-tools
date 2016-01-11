237e5468cf482e4c1d6024bb8bc60dff321d5f4f
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:28:48 +0100

    New empty activity

---
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/WriteActivity.java
@@ -0,0 +1,13 @@
+package neonlight88.com.nfctools;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class WriteActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+    }
+}
