69b95936877aa89ab1b271fbfb68f43a40538627
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:16:22 +0100

    foregroundDispatchSetup method

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
@@ -1,6 +1,10 @@
 package neonlight88.com.nfctools;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.nfc.NfcAdapter;
 import android.widget.Toast;
 
@@ -23,5 +27,21 @@ public class Tools {
                 msg,
                 Toast.LENGTH_LONG
         ).show();
+    }
+
+    public static void foregroundDispatchSetup(final Activity activity, NfcAdapter nfcAdapter, String mimeType, String[][] techList) {
+        final Intent intent = new Intent(activity.getApplicationContext(), activity.getClass());
+        intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent pendingIntent = PendingIntent.getActivity(activity.getApplicationContext(), 0, intent, 0);
+        IntentFilter[] intentFilters = new IntentFilter[1];
+        intentFilters[0] = new IntentFilter();
+        intentFilters[0].addAction(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        intentFilters[0].addCategory(Intent.CATEGORY_DEFAULT);
+        try {
+            intentFilters[0].addDataType(mimeType);
+        } catch (IntentFilter.MalformedMimeTypeException ecxeption) {
+            throw new RuntimeException("Mime type error. Please check it.");
+        }
+        nfcAdapter.enableForegroundDispatch(activity, pendingIntent, intentFilters, techList);
     }
 }
