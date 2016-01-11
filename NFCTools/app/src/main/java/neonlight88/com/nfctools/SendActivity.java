7694ad282e7dd5f5392216fb4e5ced00c60de9c6
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:43:12 +0100

    new empty activity

---
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/SendActivity.java
@@ -0,0 +1,13 @@
+package neonlight88.com.nfctools;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SendActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send);
+    }
+}
