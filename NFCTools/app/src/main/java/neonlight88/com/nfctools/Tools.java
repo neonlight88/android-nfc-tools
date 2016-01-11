fdf0bb96e7e85279652a1effa4b4ba8a3cbfe8af
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:10:18 +0100

    Added method displayToast

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
@@ -1,15 +1,27 @@
 package neonlight88.com.nfctools;
 
+import android.content.Context;
 import android.nfc.NfcAdapter;
+import android.widget.Toast;
 
 public class Tools {
 
     public static boolean checkNFC(NfcAdapter nfcAdapter) {
-        if(nfcAdapter == null) {
+        if (nfcAdapter == null) {
             return false;
         } else {
-            if(!nfcAdapter.isEnabled()) {
+            if (!nfcAdapter.isEnabled()) {
                 return false;
             }
         }
+        return true;
+    }
+
+    public static void displayToast(Context context, String msg) {
+        Toast.makeText(
+                context,
+                msg,
+                Toast.LENGTH_LONG
+        ).show();
+    }
 }
