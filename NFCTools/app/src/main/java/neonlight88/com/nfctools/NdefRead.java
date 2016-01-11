73a45b4eb37d2b8f2b70495ca22cb2c6528cf2f6
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:20:47 +0100

    Attributes and constructor

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/NdefRead.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/NdefRead.java
@@ -1,4 +1,11 @@
 package neonlight88.com.nfctools;
 
+import android.content.Context;
+
 public class NdefRead {
+    private Context context;
+
+    public NdefRead(Context context) {
+        this.context = context;
+    }
 }
