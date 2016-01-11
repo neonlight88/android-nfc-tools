db9d3e79f46d21bdf2aa3e3043ebe707d3a614cb
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 15:44:28 +0100

    New empty project named NFCTools

---
+++ NFCTools/app/src/androidTest/java/neonlight88/com/nfctools/ApplicationTest.java
@@ -0,0 +1,13 @@
+package neonlight88.com.nfctools;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
