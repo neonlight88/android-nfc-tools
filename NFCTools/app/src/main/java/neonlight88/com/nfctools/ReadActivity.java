78b2f8beae9656586eb3b356269580701515edbf
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:11:11 +0100

    class attributes

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/ReadActivity.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/ReadActivity.java
@@ -1,9 +1,13 @@
 package neonlight88.com.nfctools;
 
 import android.app.Activity;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
 
 public class ReadActivity extends Activity {
+
+    private static final String MIMETYPE = "text/plain";
+    private NfcAdapter nfcAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
