ba0e4a67403eea52e5edb647dc3d2e440884530c
Tadej Jagodnik <tadej.jagodnik@gmail.com>
2016-01-11 16:07:28 +0100

    Added method checkNFC

--- NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
+++ NFCTools/app/src/main/java/neonlight88/com/nfctools/Tools.java
@@ -1,4 +1,15 @@
 package neonlight88.com.nfctools;
 
+import android.nfc.NfcAdapter;
+
 public class Tools {
+
+    public static boolean checkNFC(NfcAdapter nfcAdapter) {
+        if(nfcAdapter == null) {
+            return false;
+        } else {
+            if(!nfcAdapter.isEnabled()) {
+                return false;
+            }
+        }
 }
